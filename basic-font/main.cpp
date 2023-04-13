29215208afcdd038f7204e32350a8d4a237247f3
CAPTAIN1947 <sayantandebin@gmail.com>
2023-04-14 03:24:12 +0530

    update

---
+++ basic-font/main.cpp
@@ -0,0 +1,43 @@
+#include <supernova/engine.h>
+#include <supernova/constants.h>
+
+using namespace std;
+
+
+
+int main(int, char**) {
+
+	Engine engine;
+
+	Window window("Font example", 800, 600);
+	Renderer renderer(window);
+
+	Clock clock;
+	Events events;
+
+	bool running = true;
+	double dt, volume;
+
+	Mouse mouse({"LEFT", "RIGHT"});
+
+	Font font("font.ttf", 30);
+	font.wrap_alignment(TTF_WRAPPED_ALIGN_CENTER);
+	Texture static_text = font.create_text(renderer, "Font Example\nSDL Powered", GREEN);
+	Rect static_text_rect = static_text.get_rect();
+	static_text_rect.size(static_text_rect.size());
+	static_text_rect.center({400, 300});
+	auto atlas = font.create_atlas(renderer, BLUE);
+
+	while (running) {
+		dt = clock.tick(60);
+
+		running = events.process_events(&EVENT_KEYS, &mouse);
+
+		renderer.clear({0, 0, 0});
+		static_text.render(static_text_rect);
+		atlas.draw_text("FPS: " + to_string(int(clock.get_fps())), {0, 0});
+		renderer.present();
+	}
+
+	return 0;
+}
