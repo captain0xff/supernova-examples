ec2ee0457e224d921b4c0283daa6b9fc8ae4e93f
CAPTAIN1947 <sayantandebin@gmail.com>
2023-10-02 03:16:26 +0530

    Added the basic-template example

---
+++ examples/basic-template/main.cpp
@@ -0,0 +1,32 @@
+#include <supernova/engine.h>
+#include <supernova/constants.h>
+
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[]) {
+
+	Engine engine;
+
+	Window window("Basic Template", 800, 600);
+	Renderer renderer(window);
+
+	Clock clock;
+	Events events;
+
+	bool running = true;
+	double dt;
+
+	while (running) {
+		dt = clock.tick(60);
+
+		running = events.process_events(&EVENT_KEYS);
+
+		renderer.clear(WHITE);
+		renderer.present();
+	}
+
+	return 0;
+}
